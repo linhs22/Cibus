b05067208e611ea3f814a669ffa6c0410cb8f90e
Liam Hayes <liamh1267@gmail.com>
2020-02-18 11:17:34 -0800

    made navbar sticky and slightly modified search page

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -4,28 +4,81 @@
 import React, { Component } from "react";
 // import Header from "../components/Header"
 // import "../components/Post/Style.css";
+import image from "./FryingPan.png"
+
+var hidden = true
+var display = "none"
 
 class Search extends Component {
+
   render() {
-    return  (
+
+
+    function checkDisplay() {   
+      hidden = !hidden   
+      if (hidden){
+        display = "none";
+      } else {
+        display = "block";
+      };
+    };
+
+    function addButtons(e){
+      // hidden = true
+      checkDisplay()
+    }
+
+
+
+
+    return (
       <div>
-      {/* <Header /> */}
-      <br/>
-      <br/>
-      <br/>
+        {/* <Header /> */}
+        <br />
+        <br />
+        <br />
         <form>
-        <br/>
-      <br/>
-      <br/>
-          <h3>Search</h3>
-          <input type="text" ref="search" placeholder="User, Tag, Food?" />
-          <br/>
-      <br/>
-      <br/>
-       
+          <br />
+          <br />
+          <br />
+          <div className="searchBar" style={{          
+            margin: "auto",
+            width: "50%",
+          }}>
+
+     
+            <h3 style={{ marginLeft: "20%" }}>Search</h3>
+
+            <input style={{ marginLeft: "20%" }}
+              type="text"
+              ref="search"
+              placeholder="User, Tag, Food?"
+              onClick={addButtons}
+            // value=""
+            />
+
+            <img style={{ height: "30px", padding: "4px", cursor: "pointer", }} src={image} alt="Frying Pan" />
+
+          </div>
+
+          {/* <div className= "buttonsDiv" style= {{display: {display}}}>
+            <button>
+              Button 1
+            </button>
+            <button>
+              Button 1
+            </button>
+            <button>
+              Button 1
+            </button>
+          </div> */}
+          <br />
+          <br />
+          <br />
+
         </form>
       </div>
     )
-    }
+  }
 }
 export default Search;
