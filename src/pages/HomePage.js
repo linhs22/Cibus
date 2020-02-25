099f2c7efa882e1795ccdd89af85a9695aafeeaf
patorrad <paolo.a.torrado@gmail.com>
2020-02-25 13:38:06 -0800

    Quick update

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -36,7 +36,7 @@ import debounce from "lodash.debounce";
   componentDidUpdate() {
     if(this.state.user && !this.state.userProps)
     {
-      this.getUsers(this.state.numOfPostToGet); 
+      this.getUsers(); 
       this.setState({userProps: true});
     }   
   }
@@ -44,21 +44,23 @@ import debounce from "lodash.debounce";
 
   handleScrollEvent = debounce((event) => {
       console.log("scrollTop: " + document.documentElement.scrollTop);
-      console.log("scrollTop: " + document.getElementById(this.state.count).offsetTop);
-      console.log("Element scrollTop: " + document.getElementById(this.state.count).offsetTop);
-      if (document.documentElement.scrollTop > document.getElementById(this.state.count).offsetTop) {
-      this.getUsers(this.state.count + this.state.usersToLoad);
+      console.log("What?: " + document.getElementById(this.state.count).offsetTop);
+      console.log("scrollTop: " + (document.getElementById(this.state.count).offsetTop - document.getElementById(1).offsetTop * 3));
+      console.log("Element scrollTop: " + (document.getElementById(this.state.count).offsetTop - document.getElementById(1).offsetTop * 3));
+      if (document.documentElement.scrollTop > document.getElementById(this.state.count).offsetTop - document.getElementById(1).offsetTop * 3) {
+      this.getUsers();
       };
   }, 100);
 
-  getUsers = (numOfPostToGet) => {
+  getUsers = () => {
       console.log("Hello");
       API.getPostsHomepage(this.state.user.id, this.state.offset)
       .then(res => {
+        console.log(res.data.length);
           this.setState({
               results: [...this.state.results, ...res.data],
               offset: this.state.offset + 2,
-              count: this.state.count + 2
+              count: this.state.count + res.data.length
           });
       })
       //.then(console.log(this.state.results + this.state.search))
@@ -68,7 +70,7 @@ import debounce from "lodash.debounce";
   render() {
     return (
       <div>
-        {this.state.results.length > 0? this.state.results.map(post => <Postcard posts={post} key={this.state.results.indexOf(post)} id={this.state.results.indexOf(post)} user={this.state.user}/>) : ""};
+        {this.state.results.length > 0? this.state.results.map(post => <Postcard posts={post} key={this.state.results.indexOf(post)} id={this.state.results.indexOf(post) + 1} user={this.state.user}/>) : ""};
         {/* {this.props.posts.results.length > 0? this.props.posts.results.map(post => <Postcard posts={post}/>) : ""}; */}
         {/* <Postcard posts={this.props.posts.results[0]}/> */}
       </div>
