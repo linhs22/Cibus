75aa4a44b0958f4024381248b0d3b84c92f27f56
Henry Lin <henrylin58@gmail.com>
2020-02-25 22:34:31 -0800

    added follower routes

--- src/components/Searchcard/index.js
+++ src/components/Searchcard/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -7,6 +7,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import API from "../../utils/API";
 
 const useStyles = makeStyles({
   root: {
@@ -20,6 +21,35 @@ const useStyles = makeStyles({
 export default function Searchcard(props) {
   const classes = useStyles();
   console.log(props)
+  const [user, setUser] = useState({
+    name: '',
+    username: '',
+    profilePic: '',
+    id: 0,
+    isAuthenticated: true
+  });
+
+  useEffect(() => {
+    API.userExist("mechea")
+      .then(res => {
+        setUser({
+          name: res.data.firstname,
+          username: res.data.username,
+          profilePic: res.data.profilePic,
+          aboutMe: res.data.aboutMe,
+          id: res.data.id
+        })
+      })
+  }, []);
+
+   const follow = () => {
+    console.log(props.user.id)
+    API.followUser(user.id, props.user.id)
+      .then(res => {
+        console.log(res)
+      })
+    }
+
   return (
     <Card className={classes.root}>
       <CardActionArea>
@@ -40,7 +70,7 @@ export default function Searchcard(props) {
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
+        <Button onClick={follow} size="small" color="primary">
           Follow
         </Button>
       </CardActions>
